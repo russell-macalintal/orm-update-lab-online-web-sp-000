2e01db23f998038a097871fcb0a8e7c46db3ed45
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-03 04:37:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,4 +69,12 @@ class Student
     end.first
   end
 
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+      SQL
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
 end
