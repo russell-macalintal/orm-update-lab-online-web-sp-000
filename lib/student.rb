38d8f7c526b40c5b3a6b5431efe6d12856750294
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-03 04:40:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,11 +52,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+    student = self.new(row[1], row[2], row[0])
+    # student.id = row[0]
+    # student.name = row[1]
+    # student.grade = row[2]
+    # student
   end
 
   def self.find_by_name(name)
