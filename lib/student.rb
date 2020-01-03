e119a2735633ff94ffba35b368dbcd81d44f7844
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-03 04:33:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,4 +59,14 @@ class Student
     student
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      Student.new_from_db(row)
+    end.first
+  end
+  
 end
